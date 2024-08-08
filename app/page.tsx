50664ec075e36246f9f904d98fe1544522147792
luis92guimaraes <luis92guimaraes@outlook.com>
2024-08-07 21:32:41 -0300

    chore: updated postcss config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "./_components/ui/button"
+import {Button} from "./_components/ui/button"
 
 const Home = () => {
-  return <button>test</button>
+  return <Button>test</Button>
 }
 
 export default Home;
